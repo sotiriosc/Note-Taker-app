d4ba2ce67a1fe88f2e65c321afd5a24ac69038d0
sotiriosc <chortogiannoss@gmail.com>
2023-03-12 19:22:50 -0400

    refactored code

--- Develop/public/routes/notes.js
+++ Develop/public/routes/notes.js
@@ -2,6 +2,7 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
+
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -25,5 +26,18 @@ notes.post('/', (req, res) => {
   }
 });
 
+
+notes.delete('/notes/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const noteToDelete = notesArray.find(el => el.id === id);
+    const index = notesArray.indexOf(noteToDelete);
+
+    notesArray.splice(index, 1);
+
+    readAndAppend(notesArray, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+//   } else {
+//     res.error('Error in adding Note'),
+});
 
 module.exports = notes;
